0a2919950a1a975de2b2ad902848e7494cd6a96b
brndngrhm <brendanvgraham@gmail.com>
2016-05-23 09:59:13 -0400

    adds installr step

--- package_mgmt/automated_package_reinstall.R
+++ package_mgmt/automated_package_reinstall.R
@@ -1,5 +1,9 @@
 #Automated Re-Install of Packages
 
+#DO FIRST PART BEFORE UPDAING r VERSION!!!
+
+----
+
 # gets the library trees within which packages are looked for
 .libPaths()
 
@@ -10,12 +14,18 @@ myPath = c("C:/Users/GRA/Documents/R/win-library/3.1")
 package_df <- as.data.frame(installed.packages(myPath))  
 
 #saves as data frame
-save(package_df, file = "packages_23MAY2016.rda") 
+save(package_df, file = "C:/Users/GRA/Desktop/Misc/R Working Directory/Other/misc/package_mgmt/packages_23MAY2016.rda")
 
 ------
+
+#update R
+library(installr)
+installr()
+
+------  
   
 #Once R is updated, load the dataframe  
-load("C:/Users/GRA/Desktop/Misc/R Working Directory/Other/misc/packages_23MAY2016.rda")
+load("C:/Users/GRA/Desktop/Misc/R Working Directory/Other/misc/package_mgmt/packages_23MAY2016.rda")
 
 #makes it a list
 package_list <- as.character(package_df$Package)
